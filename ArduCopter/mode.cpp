e6871b7c42ea28e78c7eb80a5852b829cfe2f5ff
James Morritt <james1morritt@gmail.com>
2023-09-04 13:08:42 +1200

    Updated sprint mode to be inline with loiter. Motors stay at arm speed till takeoff.

--- ArduCopter/mode.cpp
+++ ArduCopter/mode.cpp
@@ -482,10 +482,10 @@ bool Mode::_TakeOff::triggered(const float target_climb_rate) const
         return false;
     }
 
-    if (copter.motors->get_spool_state() != AP_Motors::SpoolState::THROTTLE_UNLIMITED) {
-        // hold aircraft on the ground until rotor speed runup has finished
-        return false;
-    }
+    // if (copter.motors->get_spool_state() != AP_Motors::SpoolState::THROTTLE_UNLIMITED) {
+    //     // hold aircraft on the ground until rotor speed runup has finished
+    //     return false;
+    // }
 
     return true;
 }
@@ -950,6 +950,7 @@ Mode::AltHoldModeState Mode::get_alt_hold_state(float target_climb_rate_cms)
         }
 
     } else if (takeoff.running() || takeoff.triggered(target_climb_rate_cms)) {
+        motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
         // the aircraft is currently landed or taking off, asking for a positive climb rate and in THROTTLE_UNLIMITED
         // the aircraft should progress through the take off procedure
         return AltHold_Takeoff;
@@ -959,20 +960,22 @@ Mode::AltHoldModeState Mode::get_alt_hold_state(float target_climb_rate_cms)
         if (target_climb_rate_cms < 0.0f && !copter.ap.using_interlock) {
             // the aircraft should move to a ground idle state
             motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::GROUND_IDLE);
-
-        } else {
-            // the aircraft should prepare for imminent take off
-            motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
-        }
-
-        if (motors->get_spool_state() == AP_Motors::SpoolState::GROUND_IDLE) {
-            // the aircraft is waiting in ground idle
             return AltHold_Landed_Ground_Idle;
 
         } else {
-            // the aircraft can leave the ground at any time
+            // the aircraft should prepare for imminent take off
+            motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::GROUND_IDLE);
             return AltHold_Landed_Pre_Takeoff;
         }
+
+        // if (motors->get_spool_state() == AP_Motors::SpoolState::GROUND_IDLE) {
+        //     // the aircraft is waiting in ground idle
+        //     return AltHold_Landed_Ground_Idle;
+
+        // } else {
+        //     // the aircraft can leave the ground at any time
+        //     return AltHold_Landed_Pre_Takeoff;
+        // }
 
     } else {
         // the aircraft is in a flying state
--- ArduCopter/mode_sprint.cpp
+++ ArduCopter/mode_sprint.cpp
@@ -171,15 +171,26 @@ void ModeSprint::run()
         // get pilot's desired yaw rate
         target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->norm_input_dz());
 
+        bool rng_alt_ok = copter.rangefinder_alt_ok();
+
+        Matrix3f rngRotMatrix = {
+            Vector3f{copter.ahrs.cos_pitch(),   copter.ahrs.sin_pitch() * copter.ahrs.sin_roll(),   copter.ahrs.sin_pitch() * copter.ahrs.cos_roll()}, 
+            Vector3f{0,                         copter.ahrs.cos_roll(),                             -copter.ahrs.sin_roll()},
+            Vector3f{-copter.ahrs.sin_pitch(),  copter.ahrs.cos_pitch() * copter.ahrs.sin_roll(),   copter.ahrs.cos_pitch() * copter.ahrs.cos_roll()}
+        };
+
+        int16_t fc_height_rng = copter.rangefinder_state.alt_cm_glitch_protected - (rngRotMatrix * copter.rangefinder.get_pos_offset_orient(ROTATION_PITCH_270)).z;
+
         if (
             _landing || (
-                copter.rangefinder_state.enabled && 
-                copter.rangefinder_state.alt_healthy && 
-                copter.rangefinder_state.alt_cm_glitch_protected <= (g.pilot_takeoff_alt + copter.rangefinder.ground_clearance_cm_orient(ROTATION_PITCH_270) + 10) && 
+                rng_alt_ok && 
+                fc_height_rng <= (g.pilot_takeoff_alt + (copter.rangefinder.ground_clearance_cm_orient(ROTATION_PITCH_270)) + 10) && 
                 target_climb_rate <= -get_pilot_speed_dn()*0.99 && 
                 copter.gps.ground_speed_cm() <= 50
             )
         ) {
+
+            bool cancel_landing = false;
 
             if (!_landing) {
                 _land_start_time = millis();
@@ -194,18 +205,49 @@ void ModeSprint::run()
 
             // cancel landing if throttle is not at minimum and the landing descent has not yet started
             if (_land_pause && (target_climb_rate > -get_pilot_speed_dn()*0.99 || target_yaw_rate < -500 || target_yaw_rate > 500 || copter.gps.ground_speed_cm() >= 50)) {
-                _landing = false;
+                cancel_landing = true;
             }
 
             // cancel landing if landing descent has started and high throttle cancels landing is set and throttle is high
             if (!_land_pause && (g.throttle_behavior & THR_BEHAVE_HIGH_THROTTLE_CANCELS_LAND) != 0 && copter.rc_throttle_control_in_filter.get() > LAND_CANCEL_TRIGGER_THR){
-                _landing = false;
+                cancel_landing = true;
+            }
+
+            if (_landing && _land_pause) {
+                // convert pilot input to lean angles
+                get_pilot_desired_lean_angles(target_roll, target_pitch, sprint_nav->get_angle_max_cd(), attitude_control->get_althold_lean_angle_max_cd());
+
+                // process pilot's roll and pitch input
+                sprint_nav->set_pilot_desired_acceleration(target_roll, target_pitch);
+
+#if PRECISION_LANDING == ENABLED
+                bool precision_sprint_old_state = _precision_sprint_active;
+                if (do_precision_sprint()) {
+                    precision_sprint_xy();
+                    _precision_sprint_active = true;
+                } else {
+                    _precision_sprint_active = false;
+                }
+                if (precision_sprint_old_state && !_precision_sprint_active) {
+                    // prec sprint was active, not any more, let's init again as user takes control
+                    sprint_nav->init_target();
+                }
+                // run sprint controller if we are not doing prec sprint
+                if (!_precision_sprint_active) {
+                    sprint_nav->update();
+                }
+#else
+                sprint_nav->update();
+#endif
             }
 
             if (_landing) {
                 auto_yaw.set_mode(AUTO_YAW_HOLD);
                 land_run_horizontal_control();
                 land_run_vertical_control(_land_pause);
+                if (cancel_landing) {
+                    _landing = false;
+                }
             }
 
         }
@@ -240,28 +282,16 @@ void ModeSprint::run()
             // call attitude controller
             attitude_control->input_thrust_vector_rate_heading(sprint_nav->get_thrust_vector(), target_yaw_rate);
 
-            // Matrix3f rngRotMatrix = {
-            //         Vector3f{copter.ahrs.cos_pitch(),   copter.ahrs.sin_pitch() * copter.ahrs.sin_roll(),   copter.ahrs.sin_pitch() * copter.ahrs.cos_roll()}, 
-            //         Vector3f{0,                         copter.ahrs.cos_roll(),                             -copter.ahrs.sin_roll()},
-            //         Vector3f{-copter.ahrs.sin_pitch(),  copter.ahrs.cos_pitch() * copter.ahrs.sin_roll(),   copter.ahrs.cos_pitch() * copter.ahrs.cos_roll()}
-            //     };
+            if (rng_alt_ok && fc_height_rng < (g.pilot_takeoff_alt + (copter.rangefinder.ground_clearance_cm_orient(ROTATION_PITCH_270))) - 20) {
+                // target_climb_rate = MAX(target_climb_rate, MIN(powF(fc_height_rng - g.pilot_takeoff_alt, 2) / (powF(g.pilot_takeoff_alt, 2)), 1) * g.pilot_speed_up);
+                target_climb_rate = MAX(target_climb_rate, MIN((g.pilot_takeoff_alt - fc_height_rng) / (g.pilot_takeoff_alt), 1) * g.pilot_speed_up);
 
-            // int16_t fc_height_rng = copter.rangefinder_state.alt_cm_glitch_protected - (rngRotMatrix * copter.rangefinder.get_pos_offset_orient(ROTATION_PITCH_270)).z;
-
-            int32_t alt_above_ground_cm;
-            copter.get_rangefinder_height_interpolated_cm(alt_above_ground_cm);
-
-            if (copter.rangefinder_state.enabled && copter.rangefinder_state.alt_healthy && alt_above_ground_cm < (g.pilot_takeoff_alt + copter.rangefinder.ground_clearance_cm_orient(ROTATION_PITCH_270))*0.85) {
-                // target_climb_rate = MAX(target_climb_rate, MIN(powF(alt_above_ground_cm - g.pilot_takeoff_alt, 2) / (powF(g.pilot_takeoff_alt, 2)), 1) * g.pilot_speed_up);
-                target_climb_rate = MAX(target_climb_rate, MIN((g.pilot_takeoff_alt - alt_above_ground_cm) / g.pilot_takeoff_alt, 1) * g.pilot_speed_up);
             }
-            else if (copter.rangefinder_state.enabled && copter.rangefinder_state.alt_healthy && target_climb_rate < 0) {
+            else if (rng_alt_ok && target_climb_rate < 0) {
+                target_climb_rate = MAX(target_climb_rate, MIN(powF((fc_height_rng - (g.pilot_takeoff_alt * 0.85)) / (2 * get_pilot_speed_dn()), 2) + 0.05, 1) * -get_pilot_speed_dn());
+                // target_climb_rate_mmps = MAX(target_climb_rate_mmps, MIN((alt_above_ground_mm - takeoff_landing_alt_mm) / (takeoff_landing_alt_mm / 1000), 1000) * -get_pilot_speed_dn());
 
-                //target_climb_rate = MAX(target_climb_rate, MIN(((fc_height_rng * fc_height_rng) / (3 * ((g.pilot_takeoff_alt * g.pilot_takeoff_alt) + ((get_pilot_speed_dn() * get_pilot_speed_dn()) / 2)))), 1) * -get_pilot_speed_dn());
-                // target_climb_rate = MAX(target_climb_rate, MIN(powF((alt_above_ground_cm - (g.pilot_takeoff_alt * 0.85)) / (2 * get_pilot_speed_dn()), 2) + 0.05, 1) * -get_pilot_speed_dn());
-                target_climb_rate = MAX(target_climb_rate, MIN((alt_above_ground_cm - g.pilot_takeoff_alt) / (2 * g.pilot_takeoff_alt), 1) * -get_pilot_speed_dn());
-
-                if (alt_above_ground_cm <= g.pilot_takeoff_alt + copter.rangefinder.ground_clearance_cm_orient(ROTATION_PITCH_270)) {
+                if (fc_height_rng <= g.pilot_takeoff_alt + (copter.rangefinder.ground_clearance_cm_orient(ROTATION_PITCH_270))) {
                     target_climb_rate = 0;
                 }
             }
